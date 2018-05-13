ef27c70012ea08271a280062eb82480ad217b98d
kefan <290591923@qq.com>
2018-05-14 00:23:54 +0800

    react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.count = 0
+  }
+  render(props) {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span>{this.count}</span>
+        <Button type="primary" onClick={() => this.addSolder()}>添加</Button>
       </div>
     );
+  }
+  addSolder(props) {
+    let count = this.count++;
+    this.setState({ count });
   }
 }
 
