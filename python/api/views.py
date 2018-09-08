a13fca5873fb100b7c0dff5cf8ba059feac03f35
chenpeng <kefanchen@daishujiankang.com>
2018-09-08 15:53:24 +0800

    1

--- python/api/views.py
+++ python/api/views.py
@@ -3,33 +3,45 @@ from django.http import HttpResponse, JsonResponse
 from api.models import UserInfo
 from django.core import serializers  #序列化数据
 from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import User
+from django.contrib import auth
 import json
 
-info = {'msg': '请求成功！', 'code': 1}
-data = []
+info = {'msg': '', 'code': 1}
 
 
-# Create your views here.
-def index(request):
-    # list = UserInfo.objects.all()
-    # posts_serialized = serializers.serialize('json', list)
-
-    # for i in json.loads(posts_serialized):
-    #     data.append(i['fields'])
+def create_user(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = User.objects.create_user(username=username, password=password)
+    user.is_staff = True
+    user.save()
+    info['msg'] = '注册成功！'
     info['data'] = []
-    if 'username' in request.POST:
-        user = UserInfo.objects.get(username=request.POST['username'])
-        print(user.password)
-        succ = check_password(request.POST['password'], user.password)
-        print(succ)
-        if succ:
-            info['msg'] = '登录成功！'
+    return JsonResponse(info, safe=False)
 
-        else:
-            info['msg'] = '用户名或密码错误！'
-            info['code'] = -1
+
+def set_password(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = User.objects.get(username=username)
+    user.set_password(password)
+    user.save()
+    info['msg'] = '密码修改成功!'
+    info['data'] = []
+    return JsonResponse(info, safe=False)
+
+
+def index(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        info['msg'] = '登陆成功!'
+        info['code'] = 1
     else:
-        info['msg'] = '请填写用户名或密码！'
+        info['msg'] = '用户名或密码错误,请重新输入!'
         info['code'] = -1
 
     return JsonResponse(info, safe=False)
--- src/container/home/home.js
+++ src/container/home/home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import { Form, Icon, Input, Button, Row, Col, message } from 'antd'
+// import { Form, Icon, Input, Button, Row, Col, message } from 'antd'
 import './home.scss';
 
 class home extends Component {
   componentDidMount() {
-    this.$http.get('/api/home').then(re => {
-      let res = re.data;
-      if (res.code === 1) {
-        console.log(res.data);
-      } else {
-        message.warning(<span style={{ color: '#FAAD14' }}>{res.msg}</span>)
-      }
-    })
+    // this.$http.get('/api/home').then(re => {
+    //   let res = re.data;
+    //   if (res.code === 1) {
+    //     console.log(res.data);
+    //   } else {
+    //     message.warning(<span style={{ color: '#FAAD14' }}>{res.msg}</span>)
+    //   }
+    // })
   }
   render() {
     return (
